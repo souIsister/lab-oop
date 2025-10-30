4a51ecfa76c07d18693a46a4c1c905611ea9362b
ᅠ <abaybayon24-0391@cca.edu.ph>
2025-10-30 21:06:43 +0800

    Add type hints to make_bird_sounds method

--- finals/finals3/birdCage.py
+++ finals/finals3/birdCage.py
@@ -1,4 +1,7 @@
+from typing import List
+from birds import Bird
+
 class BirdCage:
-    def make_bird_sounds(self, birds):
+    def make_bird_sounds(self, birds: List[Bird]) -> None:
         for bird in birds:
             bird.make_sound()
